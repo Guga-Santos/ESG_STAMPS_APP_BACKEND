bc5918f1b1ac407f58e95a662c2d9a0ffddc5a7f
GugaSantos <guga.santos.guga@gmail.com>
2023-09-08 16:37:46 -0300

    feat: Company route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import CompanyRoute from './routes/CompanyRoute';
 
 const app = express();
+app.use(express.json());
+app.use(CompanyRoute);
+app.use(errorHandler);
 
 export default app;
