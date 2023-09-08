d74944c0938118a5f01d29be91aab6aaa1394023
GugaSantos <guga.santos.guga@gmail.com>
2023-09-08 16:21:56 -0300

    feat: companyRoute

---
+++ src/routes/CompanyRoute.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import CompanyController from '../controllers/CompanyController';
+import Company from '../models/CompanyModel';
+import CompanyService from '../service/CompanyService';
+
+const route = Router();
+
+const company = new Company();
+const companyService = new CompanyService(company);
+const companyController = new CompanyController(companyService);
+
+route
+  .post('/company', (req, res) => companyController.create(req, res));
+
+
+export default route;
