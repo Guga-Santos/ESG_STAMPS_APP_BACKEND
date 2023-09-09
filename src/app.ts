b1f1112734211208877114de8ed39157fa296ad8
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 12:02:17 -0300

    feat: add StampRoute at app.use

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import 'express-async-errors';
 import errorHandler from './middlewares/error';
 import CategoryRoute from './routes/CategoryRoute';
 import CompanyRoute from './routes/CompanyRoute';
+import StampRoute from './routes/StampRoute';
 
 const app = express();
 app.use(express.json());
 app.use(CompanyRoute);
 app.use(CategoryRoute);
+app.use(StampRoute);
 app.use(errorHandler);
 
 export default app;
