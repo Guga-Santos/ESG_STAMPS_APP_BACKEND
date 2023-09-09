385c6239eec1d788e12a6d1af0c96604278a0d06
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 10:35:27 -0300

    feat: create category Controller method

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -1,1 +1,13 @@
-import { } from 'express';
+import { Request, Response } from 'express';
+import { ICategory } from '../interfaces/ICategory';
+import IService from '../interfaces/IService';
+
+export default class CategoryController {
+  constructor(private _service: IService<ICategory>) {};
+
+  public async create(req: Request, res: Response) {
+    const { body } = req;
+    const newCategory = await this._service.create(body);
+    return res.status(201).json(newCategory);
+  }
+}
