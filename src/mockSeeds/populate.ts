9730314577a33fe39fa2136d4fb38cafd9c42f5e
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 10:32:00 -0300

    feat: categorySeed and StampSeed methods

--- src/mockSeeds/populate.ts
+++ src/mockSeeds/populate.ts
@@ -1,6 +1,8 @@
 import { ICompany } from "../interfaces/ICompany";
+import Category from "../models/CategoryModel";
 import Company from "../models/CompanyModel";
-const companies = require('./seed.json');
+import Stamp from "../models/StampModel";
+const companies = require('./companies.json');
 
 export default class Populate {
   public companySeed() {
@@ -8,5 +10,12 @@ export default class Populate {
     companies.forEach((company: ICompany) => {
       newCompany.create(company);
     })
+  }
+  public categorySeed() {
+    const newCategory = new Category();
+  }
+
+  public StampSeed() {
+    const newStamp = new Stamp();
   }
 }
