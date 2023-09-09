106800c076ba667d4d03f6f671b154bb6cb16059
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 11:43:24 -0300

    feat: read method at StampController

--- src/controllers/StampController.ts
+++ src/controllers/StampController.ts
@@ -10,4 +10,9 @@ export default class StampController {
     const newStamp = await this._service.create(body);
     return res.status(201).json(newStamp);
   }
+
+  public async read(req: Request, res: Response) {
+    const list = await this._service.read();
+    return res.status(200).json(list);
+  }
 }
