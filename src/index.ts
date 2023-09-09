9372a3a021910b164f48a34bc6870f3938a203bb
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 10:14:34 -0300

    fix: populate at index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import app from './app';
+import Populate from './mockSeeds/populate';
 import connectToDatabase from './models/connection';
 
 const PORT = process.env.PORT || 3001;
