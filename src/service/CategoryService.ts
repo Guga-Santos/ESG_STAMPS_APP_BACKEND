2a7457d361117597582ee39fa43a520c917bb6e3
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 10:19:20 -0300

    feat: create CompanyService

---
+++ src/service/CategoryService.ts
@@ -0,0 +1,26 @@
+import { ICategory } from "../interfaces/ICategory";
+import { IModel } from "../interfaces/IModel";
+import IService from "../interfaces/IService";
+
+class CategoryService implements IService<ICategory> {
+  private _category:IModel<ICategory>;
+
+  constructor(model: IModel<ICategory>) {
+    this._category = model;
+  }
+  create(obj: unknown): Promise<{ name: string; description: string; stamps: string[]; }> {
+    throw new Error("Method not implemented.");
+  }
+  read(): Promise<{ name: string; description: string; stamps: string[]; }[] | null> {
+    throw new Error("Method not implemented.");
+  }
+  readOne(_id: string): Promise<{ name: string; description: string; stamps: string[]; }> {
+    throw new Error("Method not implemented.");
+  }
+  update(_id: string, obj: Partial<{ name: string; description: string; stamps: string[]; }>): Promise<{ name: string; description: string; stamps: string[]; } | null> {
+    throw new Error("Method not implemented.");
+  }
+  delete(_id: string): Promise<{ name: string; description: string; stamps: string[]; } | null> {
+    throw new Error("Method not implemented.");
+  }
+}
