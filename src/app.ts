e86203e852ecd0973d83f83fa1f1a7081493dd1e
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 13:48:33 -0300

    feat: add userRoute in app

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import errorHandler from './middlewares/error';
 import CategoryRoute from './routes/CategoryRoute';
 import CompanyRoute from './routes/CompanyRoute';
 import StampRoute from './routes/StampRoute';
+import UserRoute from './routes/UserRoute';
 
 const app = express();
 app.use(express.json());
 app.use(CompanyRoute);
 app.use(CategoryRoute);
 app.use(StampRoute);
+app.use(UserRoute);
 app.use(errorHandler);
 
 export default app;
