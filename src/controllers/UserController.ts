964d383cab473996f5dd8b82598826d709aba210
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 13:28:52 -0300

    feat: UserController methods

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -10,4 +10,31 @@ export default class UserController {
     const newUser = await this._service.create(body);
     return res.status(201).json(newUser);
   }
+
+  public async read(req: Request, res: Response) {
+    const list = await this._service.read();
+    return res.status(200).json(list);
+  }
+
+  public async readOne(req: Request, res: Response) {
+    const { id } = req.params;
+    const stamp = this._service.readOne(id);
+    res.status(200).json(stamp);
+  }
+
+  public async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { body } = req;
+
+    const updated = await this._service.update(id, body);
+
+    return res.status(201).json(updated);
+  }
+
+  public async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await this._service.delete(id);
+    return res.status(204).end();
+  }
 }
