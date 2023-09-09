70d20f71e51a61694437d429e615934f485383c3
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 10:36:08 -0300

    feat: readOne category Controller method

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -15,4 +15,11 @@ export default class CategoryController {
     const list = await this._service.read();
     return res.status(200).json(list);
   }
+
+  public async readOne(req: Request, res: Response) {
+    const { id  } = req.params;
+    const category = await this._service.readOne(id);
+
+    return res.status(200).json(category);
+  }
 }
