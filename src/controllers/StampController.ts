30968ff9cbd74093d55086e83dab0d93f7db587a
GugaSantos <guga.santos.guga@gmail.com>
2023-09-09 11:42:31 -0300

    feat: create method at StampController

--- src/controllers/StampController.ts
+++ src/controllers/StampController.ts
@@ -1,6 +1,13 @@
+import { Request, Response } from "express";
 import IService from "../interfaces/IService";
 import { IStamp } from "../interfaces/IStamp";
 
 export default class StampController {
   constructor(private _service: IService<IStamp>) {};
+
+  public async create(req: Request, res: Response) {
+    const { body } = req;
+    const newStamp = await this._service.create(body);
+    return res.status(201).json(newStamp);
+  }
 }
