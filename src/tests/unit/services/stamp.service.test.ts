6730b37818b2b2bf643fbcde7906891544c5a2c8
GugaSantos <guga.santos.guga@gmail.com>
2023-09-10 15:33:07 -0300

    test: stamp service create method

--- src/tests/unit/services/stamp.service.test.ts
+++ src/tests/unit/services/stamp.service.test.ts
@@ -1,23 +1,38 @@
 import Stamp from "../../../models/StampModel";
 import StampService from "../../../service/StampService";
 
+import { expect } from "chai";
 import * as sinon from 'sinon';
+import { ErrorTypes } from "../../../errors/catalog";
+import { stampMock, stampMockWithId } from "../../mocks/stampMocks";
 
 describe('Stamp Service Suite Tests', () => {
   const stampModel = new Stamp();
   const stampService = new StampService(stampModel);
 
   before(() => {
-
+    sinon.stub(stampModel, 'create').resolves(stampMockWithId);
   });
 
   after(() => {
     sinon.restore();
   });
 
-  describe('', () => {
-    it('On Success', async () => {})
-    it('On Failure', async () => {})
+  describe('Create Stamp', () => {
+    it('On Success', async () => {
+      const newStamp = await stampService.create(stampMock);
+      expect(newStamp).to.be.deep.equal(stampMockWithId);
+    })
+    it('On Failure', async () => {
+      let error;
+      try {
+        await stampService.create({});
+      } catch (err: any) {
+        error = err;
+      }
+      expect(error, 'Error should be defined').not.to.be.undefined;
+      expect(error.message).to.be.deep.equal(ErrorTypes.FieldsMissing);
+    })
   })
 
   describe('', () => {
