5286232e75f973d8b46bdd411dcbd846d7520b7b
GugaSantos <guga.santos.guga@gmail.com>
2023-09-10 11:42:48 -0300

    feat: user mocks

--- src/tests/mocks/userMocks.ts
+++ src/tests/mocks/userMocks.ts
@@ -1,5 +1,12 @@
 import { IUser } from "../../interfaces/IUser";
 
+const userMock: IUser = {
+  name: "New User",
+  email: "user@email.com",
+  password: "password",
+  role: "user"
+}
+
 const userMockWithId: IUser & { _id: string } = {
   _id: "64258edafe707e38fcdb2785",
   name: "New User",
@@ -8,4 +15,21 @@ const userMockWithId: IUser & { _id: string } = {
   role: "user"
 }
 
-export { userMockWithId };
+const updateUserMock = {
+  name: 'Updated User',
+  role: 'admin'
+}
+
+const updatedUserMock: IUser & { _id: string } = {
+  _id: "64258edafe707e38fcdb2785",
+  name: 'Updated User',
+  email: "user@email.com",
+  password: "password",
+  role: 'admin'
+}
+
+export {
+  updateUserMock,
+  updatedUserMock, userMock, userMockWithId
+};
+
