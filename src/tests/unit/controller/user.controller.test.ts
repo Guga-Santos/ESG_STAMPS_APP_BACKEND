c6795835e9667d021d37010de96a090da8ea6e30
GugaSantos <guga.santos.guga@gmail.com>
2023-09-10 15:58:19 -0300

    test: user controller create method

--- src/tests/unit/controller/user.controller.test.ts
+++ src/tests/unit/controller/user.controller.test.ts
@@ -2,8 +2,11 @@ import UserController from "../../../controllers/UserController";
 import User from "../../../models/UserModel";
 import UserService from "../../../service/UserService";
 
+
+import { expect } from "chai";
 import { Request, Response } from "express";
 import * as sinon from 'sinon';
+import { userMock } from "../../mocks/userMocks";
 
 describe('User Controller Suite Tests', () => {
   const userModel = new User();
@@ -13,14 +16,26 @@ describe('User Controller Suite Tests', () => {
   const req = {} as Request;
   const res = {} as Response;
 
-  before(() => {});
+  before(() => {
+    sinon.stub(userService, 'create').resolves(userMock);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+    res.end = sinon.stub().returns(res);
+  });
 
   after(() => {
     sinon.restore();
   })
 
   describe('Create a new User', () => {
-    it('On Success', () => {})
+    it('On Success', async () => {
+      req.body = userMock;
+      await userController.create(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(userMock)).to.be.true;
+    })
   })
 
   describe('Read all Users', () => {
