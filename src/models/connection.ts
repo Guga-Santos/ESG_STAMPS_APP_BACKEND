c4efb5497daf74f3ff52fcd903f931c2fe496b4b
GugaSantos <guga.santos.guga@gmail.com>
2023-09-11 08:53:00 -0300

    fix: harcode connection

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Selos';
+const MONGO_DB_URL = process.env.MONGODB_URI;
 
 const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGODB_URI
+  mongoDatabaseURI = process.env.MONGO_URI
     || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
