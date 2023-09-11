4c4a4b88f34d5d4693333acabb205b6ba0fc6fdd
GugaSantos <guga.santos.guga@gmail.com>
2023-09-11 09:05:56 -0300

    fix: uri

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const MONGO_DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/Selos';
 
 const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
+  mongoDatabaseURI = process.env.MONGODB_URI
     || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
